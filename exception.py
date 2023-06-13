ed13ea4a9c8e080b7e14506ae4b093030e43da80
minho <minhokim0806@gmail.com>
2023-06-13 17:53:37 +0900

    feat: Exception, try-except 문 적용

--- exception.py
+++ exception.py
@@ -3,3 +3,10 @@ class LoginException(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
+
+class NullException(Exception):
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
