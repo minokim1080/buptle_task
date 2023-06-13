ff4116beed1cbd495b66eff24bfeaa3c7c6b1c70
minho <minhokim0806@gmail.com>
2023-06-13 17:59:14 +0900

    refactor: ['key_name'] 형태 제거

--- account.py
+++ account.py
@@ -9,7 +9,7 @@ with open('./resource/account_list.json', 'r', encoding='utf-8') as f:
 def login(input_login_id):
 
     for account in account_list:
-        if account['login_id'] == input_login_id:
+        if account.get('login_id') == input_login_id:
             return input_login_id
 
     raise LoginException  # 아이디가 없을 경우 예외를 던집니다. Django 에서 핸들러로 처리할 것을 가정하고, 여기선 try-except 문을 사용하지 않습니다.
--- restaurant.py
+++ restaurant.py
@@ -17,7 +17,7 @@ def show_restaurant_list():
 
 def choose_restaurant(input_restaurant_id):
     for restaurant in restaurant_list:
-        if restaurant['id'] == input_restaurant_id:
+        if restaurant.get('id') == input_restaurant_id:
             return input_restaurant_id
 
     raise FileNotFoundError  # 입력 받은 값에 식당이 없을 경우 예외를 던집니다. Django 에서 핸들러로 처리할 것을 가정하고, 여기선 try-except 문을 사용하지 않습니다.
