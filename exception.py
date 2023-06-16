93f42e0d7e605db9233e2fbe282b0794e0a40412
minho <minhokim0806@gmail.com>
2023-06-16 17:47:19 +0900

    feat: task2 진행

--- exception.py
+++ exception.py
@@ -5,6 +5,13 @@ class LoginException(Exception):
         super().__init__(self.message)
 
 
+class LoginIdNotFoundException(Exception):
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
 class NullException(Exception):
 
     def __init__(self, message):
